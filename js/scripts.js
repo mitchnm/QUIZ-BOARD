6ce31fa55b961acbfc7f327759e4353779e25647
mitchnm <mitchndungu21@gmail.com>
2019-03-30 09:43:38 +0300

    modified every file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function onSubmit(){
   var points=0;
   var questions=5;
-  var answers=['language', 'label', 'false', 'ans3', 'parseInt']
+ 
 
 
   var question1 = document.forms['quiz']['answer1'].value;
@@ -10,10 +10,21 @@ function onSubmit(){
   var question4 = document.forms['quiz']['answer4'].value;
   var question5 = document.forms['quiz']['answer5'].value;
 
- for(var i = 1 <= questions; i++) {
-   if(eval('question' + i) == answers[i - 1]){
-     score++;
-   }
- }
- var results = documents.getElementById()
+  if(question1 === 'language')  {
+    points++
+  }
+  if(question2 === 'label')  {
+    points++
+  }
+  if(question3 === 'false')  {
+    points++
+  }
+  if(question4 === 'ans3')  {
+    points++
+  }
+  if(question5 === 'parseInt')  {
+    points++
+  }
+ 
+document.getElementById("result").innerHtml = "You got" + points + "points"
 }
