f0f07c30af99ca94281928817fb36bc7f60b7954
mitchnm <mitchndungu21@gmail.com>
2019-03-30 11:30:48 +0300

    javascript1

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,16 @@
 function onSubmit(){
-  var points=0;
-  var questions=5;
- 
+  var result=0;
+  var quiz=5;
+  var answers =['language', 'label', 'false', 'ans3', 'parseInt'];
 
 
-  var question1 = document.forms['quiz']['answer1'].value;
-  var question2 = document.forms['quiz']['answer2'].value;
-  var question3 = document.forms['quiz']['answer3'].value;
-  var question4 = document.forms['quiz']['answer4'].value;
-  var question5 = document.forms['quiz']['answer5'].value;
+  var answer1 =document.forms['quiz'][answer1].value;
+  var answer2 =document.forms['quiz'][answer2].value;
+  var answer3 =document.forms['quiz'][answer3].value;
+  var answer4 =document.forms['quiz'][answer4].value;
+  var answer5 =document.forms['quiz'][answer5].value;
 
-  if(question1 === 'language')  {
-    points++
-  }
-  if(question2 === 'label')  {
-    points++
-  }
-  if(question3 === 'false')  {
-    points++
-  }
-  if(question4 === 'ans3')  {
-    points++
-  }
-  if(question5 === 'parseInt')  {
-    points++
-  }
- 
-document.getElementById("result").innerHtml = "You got" + points + "points"
+  alert(answer3);
 }
+
+    
