528abfe476f39dc60523bbeb349a3d14127a0889
mitchnm <mitchndungu21@gmail.com>
2019-03-30 14:54:29 +0300

    javascript2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-function onSubmit(){
-  var result=0;
-  var quiz=5;
-  var answers =['language', 'label', 'false', 'ans3', 'parseInt'];
+function add(question1,question2,question3,question4,question5){
+  return question1 + question2 + question3 + question4 + question5;
+};
 
+$(document).ready(function () {
+  $("form#quiz").submit(function (event) {
+    event.preventDefault();
+    var question1 = parseInt($('input:radio[name=answer1]:checked').val());
+    var question2 = parseInt($('input:radio[name=answer2]:checked').val());
+    var question3 = parseInt($('input:radio[name=answer3]:checked').val());
+    var question4 = parseInt($('input:radio[name=answer4]:checked').val());
+    var question5 = parseInt($('input:radio[name=answer5]:checked').val());
+    var result = add(question1, question2, question3, question4, question5);
 
-  var answer1 =document.forms['quiz'][answer1].value;
-  var answer2 =document.forms['quiz'][answer2].value;
-  var answer3 =document.forms['quiz'][answer3].value;
-  var answer4 =document.forms['quiz'][answer4].value;
-  var answer5 =document.forms['quiz'][answer5].value;
+    $("#results").text(result);
 
-  alert(answer3);
-}
-
-    
+  });
+});
